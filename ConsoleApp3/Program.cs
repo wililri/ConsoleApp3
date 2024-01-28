641a38c96d976bee3f0741e470796cf783b60609
Дилюк <@DESKTOP-5SJPVS1>
2024-01-28 11:12:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x");
+
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y");
+
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double r = Math.Pow(2, -x) - Math.Cos(x) + Math.Sin(2 * x * y);
+
+            Console.WriteLine("Результат = {0:f2}",r);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a, b, c;
+
+            Console.WriteLine("Введите длину первой стороны треугольника:");
+
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите длину второй стороны треугольника:");
+
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите длину третьей стороны треугольника:");
+
+            c = double.Parse(Console.ReadLine());
+
+            double p = (a + b + c) / 2;
+
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); 
+
+            Console.WriteLine("Площадь треугольника: {0:f2}",+ area );
+        }
+    }
+}
